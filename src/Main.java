a3dd692bc7aba5d19d98aa938acf23362adebdf6
karlos <eduardokarlos187@gmail.com>
2024-09-02 22:46:37 -0300

    000

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner leitor = new Scanner(System.in);
+
+        aluno aluno = new aluno();
+
+        System.out.print("Nome: ");
+        String nome = leitor.nextLine();
+        aluno.setNome(nome);
+
+        System.out.print("1 Nota: ");
+        int nota1 = leitor.nextInt();
+        aluno.setNota1(nota1);
+
+        System.out.print("2 Nota: ");
+        int nota2 = leitor.nextInt();
+        aluno.setNota2(nota2);
+
+        System.out.println("Nome: " + aluno.getNome());
+        System.out.println("Nota 1: " + aluno.getNota1());
+        System.out.println("Nota 2: " + aluno.getNota2());
+
+        double media = (aluno.getNota1() + aluno.getNota2()) / 2.0;
+        System.out.println("Média: " + media);
     }
 }
