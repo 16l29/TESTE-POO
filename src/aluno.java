1c7aea900f4b3b4f5597313ece9dc57c24fc5af2
karlos <eduardokarlos187@gmail.com>
2024-09-02 22:44:53 -0300

    Initial commit

---
+++ src/aluno.java
@@ -0,0 +1,32 @@
+public class aluno {
+
+    private int nota1, nota2;
+    private String nome;
+
+    public aluno() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(int nota1) {
+        this.nota1 = nota1;
+    }
+
+    public int getNota2(){
+        return nota2;
+    }
+
+    public void setNota2(int nota2){
+        this.nota2 = nota2;
+    }
+}
